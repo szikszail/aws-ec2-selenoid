ff6f0611c47b87f28e417c07d4c6779fa4cc36ec
szikszail <sziklaszlo@gmail.com>
2023-12-12 10:18:45 +0100

    Added browsers

--- lib/aws-ec2-selenoid-stack.ts
+++ lib/aws-ec2-selenoid-stack.ts
@@ -37,6 +37,7 @@ export class AwsEc2SelenoidStack extends cdk.Stack {
       'usermod -a -G docker ec2-user',
       'wget https://github.com/aerokube/cm/releases/download/1.8.5/cm_linux_amd64',
       'chmod +x cm_linux_amd64',
+      './cm_linux_amd64 selenoid configure --browsers chrome,edge,safari,firefox --last-versions 4 --tmpfs 128',
       './cm_linux_amd64 selenoid start --vnc --tmpfs 128',
       './cm_linux_amd64 selenoid-ui start',
     );
